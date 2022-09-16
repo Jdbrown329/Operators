7093c2a6c3d2ec262aa302a0f4c8b62f8ebc5759
Jordan Brown <j.d.brown329@gmail.com>
2022-09-16 09:12:26 -0500

    first commit

---
+++ OperatorsExcercise/Program.cs
@@ -0,0 +1,17 @@
+﻿{
+    int a = 17, b = 4;
+    int quotient = a / b;
+    int remainder = a % b;
+
+    Console.WriteLine($"Seventeen divided by four is {quotient} and has a remainder of {remainder}.");
+}
+{
+    Console.WriteLine("Please enter the radius of your circle");
+    
+    var radius = double.Parse(Console.ReadLine());
+    
+    double answer = Math.PI * Math.Pow(radius, 2);
+
+    return; Console.WriteLine($"The area of your circle is {answer}");
+
+}
